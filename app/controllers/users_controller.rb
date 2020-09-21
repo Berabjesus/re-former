d969b1fe1f178ec323d31d9781425612ea399b0c
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-09-21 20:05:57 +0300

    ADD html form with auth token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
+
+  def user_params
+    params.permit(:username, :email, :password_digest)
+  end
+  # {"username"=>"sdfs", "email"=>"asd@easdas", "password"=>"[FILTERED]", "authenticity_token"=>"wSniYW/JnuQ770uRAoeDaQ4YmwJiFdZZJJ6TW0taBaAzYNXullZhA/e+bowoXqGDuG2ngyUeFL7IfhmkUAkJ+A=="}
+
 end
