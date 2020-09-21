c9d0c6d43418b738e0b7c0c93278a64bc3d56933
rootDEV2990 <website.dev@icloud.com>
2020-09-21 14:32:30 -0500

    change form to from_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
+    params.permit(:username, :email, :password_digest)
   end
   # {"username"=>"sdfs", "email"=>"asd@easdas", "password"=>"[FILTERED]", "authenticity_token"=>"wSniYW/JnuQ770uRAoeDaQ4YmwJiFdZZJJ6TW0taBaAzYNXullZhA/e+bowoXqGDuG2ngyUeFL7IfhmkUAkJ+A=="}
 
